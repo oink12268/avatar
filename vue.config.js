a435cc24846f933103e0db7fbe8f746e28f8587e
SUHWAN KIM <oink12268@gmail.com>
2023-01-08 20:28:21 +0900

    scss

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: `
+          @import "~@/assets/scss/_abstracts.scss";
+          @import "~@/assets/scss/_vendors.scss";
+          @import "~@/assets/scss/_common.scss";
+          @import "~@/assets/scss/_components.scss";
+        `,
+      },
+    },
+  },
 })
