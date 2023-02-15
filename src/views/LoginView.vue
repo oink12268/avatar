7022e9a82a79ff1627236d5cfae728278b693cc2
soohkim <soohkim@foodtechkorea.com>
2023-02-15 12:19:48 +0900

    facebook add

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -5,6 +5,7 @@
 			<img class="wd-150 hp-40" src="../assets/btn_google_signin_dark_focus_web.png" @click="event.click" />
 			<img class="wd-150 hp-40" src="../assets/kakao_login_medium_narrow.png" @click="event.click2" />
 			<img class="wd-150 hp-40" src="../assets/naver_login.png" @click="event.click3" />
+			<img class="wd-150 hp-40" src="../assets/naver_login.png" @click="event.click4" />
 		</div>
 	</div>
 </template>
@@ -26,6 +27,10 @@ export default {
 			click3: () => {
 				console.log('click')
 				location.href = 'http://localhost:8080/oauth2/authorization/naver'
+			},
+			click4: () => {
+				console.log('click')
+				location.href = 'http://localhost:8080/oauth2/authorization/facebook'
 			},
 		}
 
