198f6ad66eb54b642e724bbc44e9005a9470f3e2
soohkim <soohkim@foodtechkorea.com>
2023-02-22 20:24:51 +0900

    .

---
+++ src/components/layouts/Footer.vue
@@ -0,0 +1,21 @@
+<!--<template>-->
+<!--	<div class="footer-bar">-->
+<!--		<button :class="{ active: selectedService === 'order' }" @click="event.go('order')">-->
+<!--			<i class="ic24_receipt_filled" />-->
+<!--		</button>-->
+<!--		<button-->
+<!--			:class="{ active: selectedService === 'sales' }"-->
+<!--			@click="event.go('sales')"-->
+<!--		>-->
+<!--			<i class="ic24_graph_filled" />-->
+<!--		</button>-->
+<!--	</div>-->
+<!--</template>-->
+
+<!--<script>-->
+<!--export default {  -->
+<!--	name: 'Footer',-->
+<!--}-->
+<!--</script>-->
+
+<!--<style scoped></style>-->
